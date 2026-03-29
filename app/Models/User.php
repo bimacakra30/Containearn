7977333e033ec9d40bbeeb58a6274e54da72604f
Bima Cakra Bara Karebet <biimackra@gmail.com>
2026-03-29 11:31:58 +0700

    feat: New Login Role

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,39 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
+        'identity_id',
         'name',
         'email',
         'password',
+        'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function isAdmin()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return in_array($this->role, ['superadmin', 'dosen']);
+    }
+
+    public function isMahasiswa()
+    {
+        return $this->role === 'mahasiswa';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->middleware('auth');
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('mahasiswa.dashboard');
+})->middleware('auth');
+
+require __DIR__.'/auth.php';
